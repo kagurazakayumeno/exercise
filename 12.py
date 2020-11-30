22fb53958b28c4d7f21ac3cac6e0ba5b81014441
神楽坂夢乃 <kagurazakayumeno@gmail.com>
2020-12-01 00:38:50 +0800

    correct the mistake

--- 12.py
+++ 12.py
@@ -11,7 +11,7 @@ list[0].extend(["总分","平均"])
 #求学生总分和学生平均分
 for i in range(2,L):
     total1=0
-    for j in range(1,9):
+    for j in range(1,10):
         total1+=int(list[i][j])
     stu_average=total1/9
     list[i].append(str(total1))
@@ -42,4 +42,4 @@ for i in range(0,L):
     list[i]=" ".join(list[i])
 with open("result.txt","w+") as ff:
     ff.writelines(list)
-f.close
+f.close()
