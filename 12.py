17a9d493b678703e9c546c7ad62379b4908168e9
神楽坂夢乃 <kagurazakayumeno@gmail.com>
2020-12-01 09:24:29 +0800

    correct a mistake

--- 12.py
+++ 12.py
@@ -1,45 +1,45 @@
 f=open("report.txt","r+")
-list=f.readlines()
-list[0]="名次 "+list[0]
-list.insert(1,"0 平均"+" 00"*11)
-L=len(list)
+marks=f.readlines()
+marks[0]="名次 "+marks[0]
+marks.insert(1,"0 平均"+" 00"*11)
+L=len(marks)
 #化成列表
 for i in range(0,L):
-    list[i]=list[i].strip()
-    list[i]=list[i].split(" ")
-list[0].extend(["总分","平均"])
+    marks[i]=marks[i].strip()
+    marks[i]=marks[i].split(" ")
+marks[0].extend(["总分","平均"])
 #求学生总分和学生平均分
 for i in range(2,L):
     total1=0
     for j in range(1,10):
-        total1+=int(list[i][j])
+        total1+=int(marks[i][j])
     stu_average=total1/9
-    list[i].append(str(total1))
-    list[i].append(f"{stu_average:.0f}")
+    marks[i].append(str(total1))
+    marks[i].append(f"{stu_average:.0f}")
 #排序
-ele1=list.pop(0)
-ele2=list.pop(0)
-list.sort(key=lambda x:x[10],reverse=True)
-list.insert(0,ele1)
-list.insert(1,ele2)
+ele1=marks.pop(0)
+ele2=marks.pop(0)
+marks.sort(key=lambda x:x[10],reverse=True)
+marks.insert(0,ele1)
+marks.insert(1,ele2)
 for i in range(2,L):
-    list[i].insert(0,str(i-1))
+    marks[i].insert(0,str(i-1))
 #求学科平均分
 for j in range(2,13):
     total2=0
     for i in range(2,L):
-        total2+=int(list[i][j])
+        total2+=int(marks[i][j])
     sub_average=total2/(L-2)
-    list[1][j]=f"{sub_average:.0f}"
+    marks[1][j]=f"{sub_average:.0f}"
 #替换不及格
 for i in range(2,L):
     for j in range(2,10):
-        if int(list[i][j])<60:
-            list[i][j]="不及格"
+        if int(marks[i][j])<60:
+            marks[i][j]="不及格"
 #化为字符串列表
 for i in range(0,L):
-    list[i][12]+="\n"
-    list[i]=" ".join(list[i])
+    marks[i][12]+="\n"
+    marks[i]=" ".join(marks[i])
 with open("result.txt","w+") as ff:
-    ff.writelines(list)
+    ff.writelines(marks)
 f.close()
