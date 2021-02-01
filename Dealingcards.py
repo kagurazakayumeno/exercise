145925c2378be412a2af40c972973d7bf8dc34a4
神楽坂夢乃 <kagurazakayumeno@gmail.com>
2021-02-01 17:08:06 +0800

    1

---
+++ Dealingcards.py
@@ -0,0 +1,17 @@
+import random as rd
+List=[]
+List1=["♦","♣","♥","♠"]
+List2=["3","4","5","6","7","8","9","10","J","Q","K","A","2"]
+for i in range(len(List1)):
+    for j in range(len(List2)):
+        List.append(List1[i]+List2[j])
+List.append("black🃏")
+List.append("red🃏")
+rd.shuffle(List)
+player1=rd.sample(List,17)
+List1=[x for x in List if x not in player1]
+player2=rd.sample(List1,17)
+List2=[x for x in List1 if x not in player2]
+player3=rd.sample(List2,17)
+face_down_cards=[x for x in List2 if x not in player3]
+print(player1,player2,player3,face_down_cards)
